2e67500077a811fa8200f5a9a29b91f1ce880454
nikhil-gowda-t-p <nikhilgowda13121@gmail.com>
2024-02-22 16:25:18 +0530

    added global catch

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const express =require('express');
 const app = express()
 const port = 3000
 
-
+app.use(express.json()); 
 
 
 function login(req,res,next){
-  if (req.username!=user_name ||  req.paswd!=pswd){
+
+  console.log("Received request body:", req.body);
+  const user_name="Nikhil"
+  const pswd="1bc3"
+  if (req.body.username !=user_name ||  req.body.password !=pswd){
     res.status(403).json({
-      msg:"Incorrect credintials"
+      msg:"Incorrect credintials",
+      sent:req.body.username
+
     })
   }
   else{
@@ -26,10 +32,17 @@ app.get('/', (req, res) => {
 
 
 
-app.get('/homepage',login,function(req,res){
-  app.send('homepage')
+app.post('/homepage',login,function(req,res){
+  res.send('homepage')
 });
 
+//global catches 
+
+app.use(function(err,req,res,next){
+  res.json({
+    msg:"someting is wrong in server we will be back:("
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
