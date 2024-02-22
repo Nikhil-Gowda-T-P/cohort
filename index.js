bfcc6c61d12d4986f3ad385ad63423f4a28d3467
nikhil-gowda-t-p <nikhilgowda13121@gmail.com>
2024-02-22 13:15:14 +0530

    added middleware

--- index.js
+++ index.js
@@ -3,13 +3,33 @@ const express =require('express');
 const app = express()
 const port = 3000
 
+
+
+
+function login(req,res,next){
+  if (req.username!=user_name ||  req.paswd!=pswd){
+    res.status(403).json({
+      msg:"Incorrect credintials"
+    })
+  }
+  else{
+    next();
+  }
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/login',(req,res) =>{
-  res.send('login')
-})
+
+
+
+
+
+app.get('/homepage',login,function(req,res){
+  app.send('homepage')
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
